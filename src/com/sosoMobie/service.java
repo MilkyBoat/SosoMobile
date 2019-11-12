96739ae333392f3ab3e3e3aac46e75dfc9a16e78
milkyboat <1214208054@qq.com>
2019-11-12 19:45:36 +0800

    init commit

---
+++ src/com/sosoMobie/service.java
@@ -0,0 +1,13 @@
+package com.sosoMobie;/*
+ * <p>项目名称: SosoMobie </p>
+ * <p>文件名称: service.java </p>
+ * <p>描述: [类型描述] </p>
+ * <p>创建时间:  </p>
+ * @author <a href="mail to: xuyunkai@mail.nankai.edu.cn" rel="nofollow">徐云凯</a>
+ * @version v1.0
+ * @update [序号][日期YYYY-MM-DD] [更改人姓名][变更描述]
+ */
+
+abstract public class service {
+
+}
